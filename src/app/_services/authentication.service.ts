f4393816d949df08a52feb1eca2872ccf3eb4196
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2020-04-14 12:16:26 +0200

    Ran code formatter

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,44 +1,44 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+﻿import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
-import { environment } from '@environments/environment';
-import { User } from '@app/_models';
+import {environment} from '@environments/environment';
+import {User} from '@app/_models';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthenticationService {
-    private currentUserSubject: BehaviorSubject<User>;
-    public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-    constructor(private http: HttpClient) {
-        this.currentUserSubject = new BehaviorSubject<User>(null);
-        this.currentUser = this.currentUserSubject.asObservable();
-    }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(null);
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    public get currentUserValue(): User {
-        return this.currentUserSubject.value;
-    }
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
 
-    public get authToken():string {
-      // TODO
-      return "";
-    }
+  public get authToken(): string {
+    // TODO
+    return '';
+  }
 
-    login(username: string, password: string) {
-        return this.http.post<any>(`${environment.apiUrl}/auth/login`, { username, password })
-            .pipe(map((user : User) => {
-                // login successful
-                this.currentUserSubject.next(user);
-                return user;
-            }));
-    }
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/auth/login`, {username, password})
+      .pipe(map((user: User) => {
+        // login successful
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
 
-    logout() {
-      return this.http.post<any>(`${environment.apiUrl}/auth/logout`, {})
-        .subscribe(() => {
-          // logout successful
-          this.currentUserSubject.next(null);
-        });
-    }
+  logout() {
+    return this.http.post<any>(`${environment.apiUrl}/auth/logout`, {})
+      .subscribe(() => {
+        // logout successful
+        this.currentUserSubject.next(null);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
-﻿import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+﻿import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { appRoutingModule } from './app.routing';
+import {AppComponent} from './app.component';
+import {appRoutingModule} from './app.routing';
 
-import { ErrorInterceptor } from './_helpers';
-import { HomeComponent } from './home';
-import { LoginComponent } from './login';
+import {ErrorInterceptor} from './_helpers';
+import {HomeComponent} from './home';
+import {LoginComponent} from './login';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        appRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        LoginComponent
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-    ],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
