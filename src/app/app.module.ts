786d10ec2288c1dfbe1672326d02767e2b78082b
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2020-04-27 16:31:52 +0200

    Redesigned navbar and login page (fixes #11)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,18 @@ import {WebsocketService} from '@app/_services/websocket.service';
 import {AuthenticationInterceptor} from '@app/_helpers/authentication.interceptor';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatOptionModule} from '@angular/material/core';
 
 @NgModule({
   imports: [
@@ -20,7 +32,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     HttpClientModule,
     appRoutingModule,
-    BrowserAnimationsModule],
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatProgressSpinnerModule
+  ],
   declarations: [
     AppComponent,
     HomeComponent,
