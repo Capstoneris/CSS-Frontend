84414c004c906f0645564e4158b678231165c6d6
Amir H. E <piano337@yahoo.de>
2020-05-09 22:45:52 +0200

    Added dynamic list with validators

--- src/app/example-form/example-form.component.ts
+++ src/app/example-form/example-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, FormArray, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: './example-form.component.html',
@@ -14,7 +14,39 @@ export class ExampleFormComponent implements OnInit {
   ngOnInit(): void {
     this.exampleForm = this.formBuilder.group({
       name: '',
-      message: 'test default message'
+      message: 'test default message',
+      phones: this.formBuilder.array([])
     });
+  }
+
+  get phoneForms(){
+    return this.exampleForm.get('phones') as FormArray;
+  }
+
+  getPhonePrefix(i){
+    return this.phoneForms.at(i).get('prefix');
+  }
+
+  getPhoneNumber(i){
+    return this.phoneForms.at(i).get('number');
+  }
+
+  addPhone(){
+    const phone = this.formBuilder.group({
+      prefix:['', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]],
+      number:['', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]],
+    })
+
+    this.phoneForms.push(phone);
+  }
+
+  deletePhone(i){
+    this.phoneForms.removeAt(i);
   }
 }
