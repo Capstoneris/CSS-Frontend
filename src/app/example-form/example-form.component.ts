3f2133c9fefd72fd4712050ac2c99230b533aeb4
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2020-05-24 19:03:50 +0200

    Fixed build errors

--- src/app/example-form/example-form.component.ts
+++ src/app/example-form/example-form.component.ts
@@ -45,8 +45,8 @@ export class ExampleFormComponent implements OnInit, AfterViewInit {
 
   private fieldLinks: Map<string, HTMLElement> = new Map<string, HTMLElement>();
 
-  constructor(private formBuilder: FormBuilder, private websocketService: WebsocketService,
-              private authenticationService: AuthenticationService) {
+  constructor(private formBuilder: FormBuilder, public websocketService: WebsocketService,
+              public authenticationService: AuthenticationService) {
   }
 
   ngOnInit(): void {
