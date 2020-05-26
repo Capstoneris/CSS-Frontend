cc296f8adb92f529b7f738b4302bb195b238cbdb
Amir H. E <piano337@yahoo.de>
2020-05-26 23:46:20 +0200

    Fixing linting issues

--- src/app/example-form/example-form.component.ts
+++ src/app/example-form/example-form.component.ts
@@ -173,11 +173,11 @@ export class ExampleFormComponent implements OnInit, AfterViewInit {
   // Only hue is generated randomly.
   // saturation = 100%, lightness = 80% always.
   generateRandomColor() : void {
-    let min : number = 0;
-    let max : number = 360;
+    const min = 0;
+    const max = 360;
     // Min and max are included!
-    let hValue : number = Math.floor(Math.random() * (max - min + 1) + min);
-    this.hslColor = "hsl(" + hValue + ", 100%, 80%)";
+    const hValue = Math.floor(Math.random() * (max - min + 1) + min);
+    this.hslColor = 'hsl(' + hValue + ', 100%, 80%)';
   }
 
 }
